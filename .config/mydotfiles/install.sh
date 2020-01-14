0ea40382c71f0111401316c5cdfcef9f2849dd0c
Kerry Cerqueira <kerrycerqueira@gmail.com>
2020-01-14 16:37:52 +0000

    changed AUR ohmyzsh to upstream repo

--- .config/mydotfiles/install.sh
+++ .config/mydotfiles/install.sh
@@ -26,7 +26,7 @@ trap cleanup EXIT
 
 #Install pulseaudio
 install_pulse && rm -rf "$HOME"/.config/pulse
-pulseaudio --kill && pulseaudio --start
+pulseaudio --start
 
 #Install pacman installable packages
 sudo pacman -Syu neovim python-pynvim alacritty ranger biber steam xournalpp firefox zsh rofi texlive-most zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps pkgfile
@@ -40,15 +40,16 @@ git clone https://github.com/alexanderjeurissen/ranger_devicons.git "$HOME"/.con
 
 # Miniconda
 mkdir "$WORK_DIR"/miniconda && curl -o "$WORK_DIR"/miniconda/install.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-cd "$WORK_DIR"/miniconda; chmod u=rxw install.sh; ./install.sh -b -p "$HOME"/conda;
+cd "$WORK_DIR"/miniconda; chmod u=rxw install.sh; ./install.sh -b
 
 # Create conda environments
 source "$HOME"/conda/bin/activate; conda init
+conda config --add channels conda-forge
 conda update -y conda; conda install -y jupyter ipykernel jupyterlab; conda create -y -n sage sage python=2.7; conda create -y -n anaconda anaconda
 
 # R Studio and R jupyter kernel
 git clone --depth=1 https://aur.archlinux.org/rstudio-desktop-bin.git "$WORK_DIR"/rstudio
-cd "$WORK_DIR"/rstudio; makepkg -is --no-confirm
+cd "$WORK_DIR"/rstudio; makepkg -is --noconfirm
 R -e "install.packages('IRkernel', lib='$HOME/R/templib', repos='https://cloud.r-project.org/'); library('IRkernel', lib.loc='$HOME/R/templib'); installspec(name='irkernel', displayname='R kernel')"
 
 # Sourcecode pro font
@@ -56,8 +57,11 @@ git clone --depth=1 https://aur.archlinux.org/nerd-fonts-source-code-pro.git "$W
 cd "$WORK_DIR"/sourcecodepro; makepkg -is
 
 # oh-my-zsh and some plugins
-git clone https://aur.archlinux.org/oh-my-zsh-git.git "$WORK_DIR"/oh-my-zsh
-cd "$WORK_DIR"/oh-my-zsh; makepkg -is
+cd "$WORK_DIR"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# deprecated: removed AUR oh-my-zsh in favor of git clone of the upstream repo
+# git clone https://aur.archlinux.org/oh-my-zsh-git.git "$WORK_DIR"/oh-my-zsh
+# cd "$WORK_DIR"/oh-my-zsh; makepkg -is
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME"/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions "$HOME"/.oh-my-zsh/custom/plugins/zsh-completions
